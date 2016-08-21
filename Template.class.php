ee4f14a7fd2bcd0f6f346404075948a5732a13f8
Max <smeex@mail.ru>
2016-08-21 16:41:24 +0300

    Refactoring

--- Template.class.php
+++ Template.class.php
@@ -78,30 +78,3 @@
 			}
 		}
 	}
-
-	try {
-		$templater = new Templater();
-		$templater->loadFromStr("<!DOCTYPE html>
-		<html lang=\"{LANG}\">
-		<head>
-			<meta charset=\"{CHARSET}\">
-			<title>{TITLE}</title>
-			{MEDIA_AFTER}
-		</head>
-		<body>
-			{CONTENT}
-		</body>
-			{MEDIA_BEFORE}
-		</html>");
-		$templater->assets( ["LANG", "charset"], ["en", "UTF-8"] );
-		$templater->assets( "title", "Document title" );
-		$templater->assets( [
-			"media_after" => null,
-			"media_before" => null
-		], null, Templater::ASSOCIATIVE_ARRAY);
-		$templater->assets( "content", "<p>Hello world</p>" );
-
-		echo $templater->toString();
-	} catch (Exception $e) {
-		echo $e->getMessage();
-	}
